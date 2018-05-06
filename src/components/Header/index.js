21148c0dfe9fb6f9da1380c8c620534e0e0e2f83
Young Kim <aprilrd8943@gmail.com>
2018-05-06 19:09:47 +0900

    Add navigation to the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,7 @@ const Header = () => (
       style={{
         margin: "0 auto",
         maxWidth: "42rem",
-        padding: "1.5rem 1.125rem",
+        padding: "1.5rem 1.125rem 0rem",
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -23,6 +23,34 @@ const Header = () => (
         </Link>
       </h1>
       <div>On Humans, Javascript, and Tech</div>
+      <div>
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          Home
+        </Link>{" "}
+        |{" "}
+        <Link
+          to="/tags"
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          Tags
+        </Link>{" "}
+        |{" "}
+        <Link
+          to="/daily-readings"
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          Daily Readings
+        </Link>
+      </div>
     </div>
   </div>
 );
